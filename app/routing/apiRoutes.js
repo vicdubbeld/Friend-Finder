c5f7695cb9c3458a1c31fdb17972fc363d3a6fbe
vicdubbeld <vicdubbeld@gmail.com>
2019-05-17 15:28:06 -0400

    apiRoute.js functions not right yet

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,18 @@
-// * A GET route with the url `/api/friends`. 
-// This will be used to display a JSON of all possible friends.
-app.get("/api/friends", function(req, res) {
-    return res.json(friends);
-});
+module.exports = function(app) {
+  // * A GET route with the url `/api/friends`.
+  // This will be used to display a JSON of all possible friends.
+  app.get("/api/friends", function(req, res) {
+      
+  });
+  // * A POST routes `/api/friends`. This will be used to handle incoming survey results.
+  // This route will also be used to handle the compatibility logic.
+  app.post("/api/friends", function(req, res) {
+    var newFriend = req.body;
 
-// * A POST routes `/api/friends`. This will be used to handle incoming survey results. 
-// This route will also be used to handle the compatibility logic. 
-app.post("/api/friends", function(req, res) {
-    return res.json(results);
-    
-});
+    console.log(newFriend);
+
+    friends.push(newFriend);
+
+    res.json(newFriend);
+  });
+};
